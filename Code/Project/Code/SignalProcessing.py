c845067d33a2c8af04cb9a55a80d43618edd511c
GlobalTraveler <caspervanelteren@gmail.com>
2017-01-18 13:54:51 +0100

    prevent overwriting files

--- Code/Project/Code/SignalProcessing.py
+++ Code/Project/Code/SignalProcessing.py
@@ -8,28 +8,34 @@ from h5py import File, special_dtype
 from pylab import *
 # from bufferInterface import bufhelp
 import bufhelp
+import os
+import csv
+
 
 # connect to buffer
 ftc, hdr = bufhelp.connect()
 
+# get cap file
+filepath = '../../resources/caps/cap_tmsi_mobita_im.txt'
+capfile = np.loadtxt(filepath, dtype = str)
+# print(capfile)
+# print(type(capfile))
 # SET THE SUBJECT
-num = 0
+num = 3
 # storage for the data
 dataDir = '../Data/'
-fileCalibration = dataDir + 'calibration_subject_{0}.hdf5'.format(num)
-import os
-if os.path.isfile(fileCalibration):
-    num = raw_input('Please enter a new subject number')
-    fileCalibration = dataDir + 'calibration_subject_{0}.hdf5'.format(num)
-fileTest  = dataDir + 'test_subjecet_{0}'.format(num)
-print('Storing data in: \n\t ', fileCalibration)
 
-# p300 occurs up to 500 ms after stimulus
+# fileTest  = dataDir + 'test_subjecet_{0}'.format(num)
+
+
+# P300 occurs up to 500 ms after stimulus
 # taking 100 ms more than expectation
 trlen_ms = 600
 run = True
+
+# calibrationCatchEvents is list of [type, value]
 calibrationCatchEvents = [\
-'target', 'feet', 'target', 'right hand', 'target', 'left hand']
+'target', 'feet', 'target', 'right hand', 'target', 'left hand', 'feedback','positive','feedback','negative']
 print("Waiting for start event.")
 while run:
     e = bufhelp.waitforevent('start',1000, 0)
@@ -37,6 +43,15 @@ while run:
     # print(':',type(e))
     # print(e)
     if e is not None:
+        while True:
+            fileCalibration = dataDir + 'calibration_subject_{0}.hdf5'.format(num)
+            if os.path.isfile(fileCalibration):
+                num = raw_input('Please enter a new subject number ')
+                fileCalibration = dataDir + 'calibration_subject_{0}.hdf5'.format(num)
+            else:
+                break
+        print('Storing data in: \n\t ', fileCalibration)
+
         # print(e[0], e[1])
         # print('Found event')calibration
         if e.value == "calibration":
@@ -51,6 +66,7 @@ while run:
             data            = np.array(data)
             dt              = special_dtype(vlen=bytes)
             ev              = np.array([(event.type, event.value) for event in events])
+            # specify [lowerBound, upperBound] bandpass filter range
             filterBand      = [0, 60]
             processedData   = preproc.stdPreproc(data, filterBand, hdr)
             with File(fileCalibration,'w') as f:
@@ -58,6 +74,7 @@ while run:
                 f.create_dataset('rawData', data = data)
                 f.create_dataset('events', data = ev, dtype = dt)
                 f.create_dataset('processedData', data = processedData)
+                f.create_dataset('cap', data = capfile)
                 # f.create_dataset()
 
             print("End calibration phase")
@@ -65,60 +82,41 @@ while run:
         elif e.value == "train":
             print("Training classifier")
             # linear detrend, filter, average across epochs
-            # to be on the save side i picked a normal range;
-            # there is some evidence that p300 is within the theta / alpha band.
-            # Here i choose a wide band pass that would be able to catch it
-            # and supress high frequency noise.
-            # model = classification.trainClassifier(fileCalibration, hdr.fSample)
+            # to be on the save side I picked a normal range;
+            # there is some evidence that P300 is within the theta / alpha band
+            # here I choose a wide band pass that would be able to catch it
+            # and suppress high frequency noise.
+            #model = classification.trainClassifier(fileCalibration, hdr.fSample)
             bufhelp.sendEvent("training","done")
 
         elif e.value =="test":
             print("Feedback phase")
             keep = True
-            fig, ax = subplots()
-
-            tmp = zeros((4, 200))
-            ylim([-1,1])
-            p = ax.plot(tmp.T)
-            show(0)
-            print(len(p))
-
-
             while keep:
-                s = ftc.getHeader().nSamples - 1
-                dat = ftc.getData((s,s))
-                tmp = np.roll(tmp, -1)
-                tmp[:,-1] = dat
-                [pi.set_ydata(tmp[idx, :]) for idx, pi in enumerate(p)]
-                pause(1e-10)
-                draw()
-
-
-
-
-                # data, events, stopevents = bufhelp.gatherdata(["stimulus"],\
-                #                                             trlen_ms,[("run","end"),\
-                #                                             ('test', 'end')], \
-                #                                             milliseconds=True, verbose = False)
-                # if stopevents.type == 'test' and stopevents.value == 'end':
-                #     keep = False
-                # else:
-                #     filterBand = [0, 60]
-                #     # linear detrend, filter, average across epochs
-                #     processedData = preproc.stdPreproc(data, filterBand, hdr)
-                #     pred = FeedbackSignals.genPrediction(processedData, model, events)
-                #     # sanity check
-                #     print('prediction', pred)
-                #     bufhelp.sendEvent("classifier.prediction", pred)
-                #     data = np.array(data)
-                #     ev              = np.array([(event.type, event.value) for event in events])
-                #     # save the test phase
-                #     with File(fileTest,'w') as f:
-                #         # f.create_dataset('targets', data = tmp)
-                #         f.create_dataset('rawData', data = data)
-                #         f.create_dataset('events', data = ev, dtype = dt)
-                #         f.create_dataset('processedData', data = processedData)
-                #         # f.create_dataset()
+                data, events, stopevents = bufhelp.gatherdata(["stimulus"],\
+                                                            trlen_ms,[("run","end"),\
+                                                            ('test', 'end')], \
+                                                            milliseconds=True, verbose = False)
+                if stopevents.type == 'test' and stopevents.value == 'end':
+                    keep = False
+                else:
+                    # specify [lowerBound, upperBound] bandpass filter range
+                    filterBand = [0, 60]
+                    # linear detrend, filter, average across epochs
+                    processedData = preproc.stdPreproc(data, filterBand, hdr)
+                    pred = FeedbackSignals.genPrediction(processedData, model, events)
+                    # sanity check
+                    print('prediction', pred)
+                    bufhelp.sendEvent("classifier.prediction", pred)
+                    data = np.array(data)
+                    ev              = np.array([(event.type, event.value) for event in events])
+                    # save the test phase
+                    with File(fileTest,'w') as f:
+                        # f.create_dataset('targets', data = tmp)
+                        f.create_dataset('rawData', data = data)
+                        f.create_dataset('events', data = ev, dtype = dt)
+                        f.create_dataset('processedData', data = processedData)
+                        # f.create_dataset()
 
         elif e.value =="exit":
             run = False
--- Code/Project/Code/calibration.py
+++ Code/Project/Code/calibration.py
@@ -5,10 +5,17 @@ from pylab import *
 
 # PARAMETERS
 # prespecified number of targets
-nTrials             = 5
-# targets
+nTrials             = 60
+# timestep
 tmp                 = .1
-target_duration     = 10     # controls how long to show a target
+# controls how long to show a target (is multiplied with tmp)
+target_duration     = 20
+# controls how long to show the feedback (is multiplied with tmp)
+feedback_duration   = 10
+# controls how long to do nothing
+rest_duration = 15
+# proportion of negative feedback given
+proportion_negative = 0.33
 
 '''
 Note to reader; there are some random numbers in this script; these are
@@ -19,24 +26,18 @@ mainly related to visual and thus can be tweaked to whatever we like
 # connect with the buffer
 import bufhelp
 fct, hdr = bufhelp.connect()
+
 bufhelp.sendEvent('start', 'calibration')
 
 close('all')
 
 circleLabels = [ 'feet' , 'right hand', 'left hand']
 
-print(hdr.labels)
-
 fig, ax = subplots(1,1)
-
+subplots_adjust(left=0, right=1, top=1, bottom=0)
 # from matplotlib import pyplot as plt
 mng = get_current_fig_manager()
 mng.full_screen_toggle()
-subplots_adjust(left=0, right=1, top=1, bottom=0)
-import matplotlib as mpl
-mpl.rcParams['toolbar'] = 'None'
-# fig.canvas.full_screen_toggle()
-# show()
 
 r = 2
 nCircle = len(circleLabels)  + 1
@@ -62,6 +63,16 @@ def press(event):
     if event.key == ' ':
         start = 1
 
+def waitForSpacePress():
+    global start
+    start = 0
+    alpha = True
+    while alpha:
+        if start == 1:
+            alpha = False
+            text.set_visible(0)
+        pause(.1)
+
 # set background
 ax.set_facecolor('black')
 # remove labels from axes
@@ -72,11 +83,17 @@ ax.set_yticks([])
 # remove middle circle
 nCond = len(circleLabels)
 
+# create feedback vector
+cutoff = nTrials*proportion_negative
+give_feedback = np.zeros((nTrials),dtype = int)
+give_feedback[:int(cutoff)] = 1
+np.random.shuffle(give_feedback)
+
 # create target vectors
 conditionLength = nTrials // nCond
 targets = np.ones((nTrials,),dtype = int)
 # the idea here is to create 4 labels of equal size,
-# later on arbritrarily i took 0 to be the false trials
+# later on arbitrarily i took 0 to be the false trials
 # the rest will be correct  trials
 for i in range(nCond):
     targets[i * conditionLength: (i + 1) * conditionLength] = i
@@ -84,7 +101,8 @@ for i in range(nCond):
 np.random.shuffle(targets)
 fig.canvas.mpl_connect('key_press_event', press)
 
-# welcome text
+# display welcome text
+# create a figure which is full screen in first place TODO
 text = ax.text(0, 0,\
 'Welcome\n Press space to start',\
 color = 'white',\
@@ -92,16 +110,13 @@ horizontalalignment = 'center',\
 verticalalignment = 'center')
 
 # colors
-resetcolor = 'gray'
-targetcolor = 'slategray'
+resetcolor = '#404040'
+targetcolor = '#a6a6a6'
+centercolor = '#a6a6a6'
+textcolor = 'white'
+restingcolor = 'white'
 
-start = 0
-alpha = True
-while alpha:
-    if start == 1:
-        alpha = False
-        text.set_visible(0)
-    pause(.1)
+waitForSpacePress()
 
 # set up the circle objects
 center =  (0,0)
@@ -114,27 +129,71 @@ for i in range(nCircle):
     else:
         c = Circle(coordinate, r / 4., color = resetcolor)
         ax.text(coordinate[0], coordinate[1], circleLabels[i],\
-        color = 'white',  horizontalalignment='center', verticalalignment='center')
+        color = textcolor,  horizontalalignment='center', verticalalignment='center')
     circles.append(c)
     ax.add_artist(circles[i])
 
 
-for target in targets:
-    # send event to buffer
+for idx,target in enumerate(targets):
+    if (idx%15==0) and idx>0:
+        ax.cla()
+        # display break text
+        # create a figure which is full screen in first place TODO
+        text_str = 'Take a short break\n This was trial ' + str(idx) + ' of ' + str(len(targets)) + '\n Press space to start'
+        text = ax.text(0, 0,text_str,\
+        color = 'white',\
+        horizontalalignment = 'center',\
+        verticalalignment = 'center')
+        waitForSpacePress()
+
+        # set up the circle objects
+        center =  (0,0)
+        circles = []
+        for i in range(nCircle):
+            coordinate = (np.sin(angles[i]), np.cos(angles[i]))
+            # the last one is the one that moves
+            if i == nCircle-1:
+                c = Circle(center, r / 8., color = resetcolor)
+            else:
+                c = Circle(coordinate, r / 4., color = resetcolor)
+                ax.text(coordinate[0], coordinate[1], circleLabels[i],\
+                color = textcolor,  horizontalalignment='center', verticalalignment='center')
+            circles.append(c)
+            ax.add_artist(circles[i])
+
+
+
+    # send event with target label to buffer'
+    bufhelp.sendEvent('target', 'rest' )
+    # update color of circles
+    circles[-1].center = center
+    circles[-1].update({'color':restingcolor})
+    circles[int(target)].update({'color': resetcolor})
+
+    fig.canvas.draw()
+
+    # run rest
+    for i in range(rest_duration):
+        fig.canvas.draw()
+        pause(tmp)
+
+
+    # send event with target label to buffer
     bufhelp.sendEvent('target', circleLabels[target] )
     # update center from middle circle
     circles[-1].center = center
     circles[-1].update({'color': targetcolor})
-    circles[int(target)].update({'color':targetcolor})
 
     fig.canvas.draw()
 
     pause(tmp)
-    circles[-1].update({'color':'white'})
+    circles[-1].update({'color':centercolor})
     circles[int(target)].update({'color': resetcolor})
 
     # run a target
     for i in range(target_duration):
+        # Color target while animation
+        circles[int(target)].update({'color':targetcolor})
         rand_move = np.random.randn()*2*np.pi
         coord = (.1 * np.sin(rand_move), .1 *np.cos(rand_move))
         circles[-1].center = coord
@@ -142,13 +201,22 @@ for target in targets:
         pause(tmp)
 
     circles[-1].center = center
-    # show  right wrong
-    if target != 0:
+    circles[int(target)].update({'color':resetcolor})
+
+    # show feedback
+    # different feedback distribution needed? TODO
+
+    if give_feedback[idx] == 1:
         circles[-1].update({'color':'red'})
+        # send event with feedback label to buffer
+        bufhelp.sendEvent('feedback', 'negative')
     else:
         circles[-1].update({'color':'green'})
+        # send event with feedback label to buffer
+        bufhelp.sendEvent('feedback', 'positive' )
+
     fig.canvas.draw()
-    pause(tmp)
+    pause(tmp*feedback_duration)
 fig.clf()
 bufhelp.sendEvent('calibration','end')
 
