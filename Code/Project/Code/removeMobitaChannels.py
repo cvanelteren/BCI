f962799ea84c3cc3f0e0c6ead1f54d8a2fb308a1
GlobalTraveler <caspervanelteren@gmail.com>
2017-01-18 19:56:03 +0100

    mobita channels removed: a script can do this now, signal processing is edited to accomodate for the future

--- Code/Project/Code/removeMobitaChannels.py
+++ Code/Project/Code/removeMobitaChannels.py
@@ -1,18 +1,18 @@
-from shutil import move
 from h5py   import File
-
+import os
+from shutil import move
 '''
 This is a temporary script to remove the number of channels from the
 data we already have.
 We had 37 output by the mobita, but we only need the first channels as dictated
-by the cap file
+by the cap file. This is fixed within the signal processing script
 '''
 def removeChannels(fileDir):
     # remove the lab
     newStorage  = fileDir
     a, b        = newStorage.split('_LAB')
     newStorage  = a + b
-    with File(fileDir) as f:
+    with File(fileDir, 'r') as f:
         for i in f: print(i)
         rawData     = f['rawData'].value
         procData    = f['processedData'].value
@@ -22,15 +22,22 @@ def removeChannels(fileDir):
     nChans      = caps.shape[0]
     newData     = [tmp[:, :, :nChans] for tmp in [rawData, procData]]
     file        = fileDir.split('/')[-1]
-    # print(file)
-    # shutil.move(fileDir, 'Backups/' + file)
-    with File(newStorage) as f:
+    # move file to backup
+    move(fileDir, os.path.realpath('../Data/Backup/' + file))
+    with File(newStorage,'w') as f:
         labels = ['rawData', 'processedData','cap']
         data   = [newData[0], newData[1], caps]
-        for label, datai in zip(data, labels):
-            print(label, datai)
+        for label, datai in zip(labels, data):
+            f.create_dataset(label, data = datai)
 
 
 
 
-removeChannels('../Data/calibration_subject_5_LAB.hdf5')
+
+removeChannels('../Data/calibration_subject_4_LAB.hdf5')
+
+# sanity check
+# with File('../Data/calibration_subject_5.hdf5') as f :
+#     for i in f: print(i)
+#     data = f['processedData'].value
+#     print(data.shape)
